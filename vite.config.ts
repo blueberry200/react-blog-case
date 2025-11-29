83557d756109370ca2a9ef5db5ec3d209b2531d1
blueberry200 <blueberry4006@gmail.com>
2025-11-29 21:35:56 +0800

    add yml

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
+const isProd = process.env.NODE_ENV === "production";
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -10,4 +12,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: isProd ? "/vue-blog-case/" : "/",
 });
